305b2b86cc2efdb0afff7505e758913b7bd60aaf
Nick Loveridge <nick@fusemedia.ca>
2018-10-29 08:59:15 -0700

    Wrap emoji in span>

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
   <Layout>
     <div className="flex items-center h-full">
       <div className="w-full md:w-1/3 rounded shadow p-4 bg-white mx-2 md:mx-auto">
-        <h1 className="text-center mb-2">Hello Friend 👋</h1>
+        <h1 className="text-center mb-2">Hello Friend <span role="img">👋</span></h1>
         <h2 className="text-center font-normal text-grey-dark text-lg">Welcome to the Gatsby Tailwind Starter</h2>
       </div>
     </div>
